f0f41292404fb8f0e055cf2c9d1537df9cc363cf
Emma ;) <ejennings13@my.bcit.ca>
2024-10-11 16:02:26 -0700

    Changed README and made mods to stables.js (Lab 6)

--- README.md
+++ README.md
@@ -2,19 +2,22 @@
 ---
 Emma Jennings
 ---
-This is *so* **epic**
+This is *so* **awesome**
 ~~help 😭~~
 ------
-3 aha moments: 
->1. When the teacher was showing us the shortcuts!!
->2. When the teacher was explaining the difference between camelCase and snake_case
->3. When the teacher showed us the HTML, CSS, and Java analogy
+3 confusing things: 
+>1. When to delete branches 
+>2. How to properly format/arrange your code to Industry Standard 
+>3. Why we store things in Arrays 
 ----
-2 things that are still unclear 🙃
->1. How Git on my computer adds my file into GitHub
->2. What all the shortcuts are
+3 things that I have a solid understanding of 🙃:
+>1. How to open VSCode with the Terminal!
+>2. How to make new branches and navigate them!
+>3. Why we start counting at 0 and not 1 for Indexes!
 ----
-1 Word for how you're feeling
->1. Excited!
+1 Analogy that I use:
+>1. Comparing a git branch to a fork in the road
 ------
-![sam which](https://www.onceuponachef.com/images/2024/01/Grilled-Cheese-6-760x1140.jpg)
+![pumpkin cat](https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNXR4NmRoMjNuMHB3N2NjcGF1OHlsZmExNzE1c2x0c2gyaGV5Znl5NCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3Hze58BTfILLIeRLmh/giphy.gif)
+
+
--- stables.js
+++ stables.js
@@ -1,3 +1,4 @@
+    //Lesson-123 Code
 let horseName = "Jenis";
 console.log(horseName);
 let secondHorseName = "Kenis";
@@ -8,7 +9,8 @@ let isHorseInside = false;
 console.log(isHorseInside);
 
 
-//Lesson-4 Code
+
+    //Lesson-4 Code
 console.log(typeof horseAge)
 const STABLE_MONTHLY_FEE = 2000 // uppercase is preferred for constants
 console.log(STABLE_MONTHLY_FEE); 
@@ -16,7 +18,7 @@ console.log(STABLE_MONTHLY_FEE);
 console.log(STABLE_MONTHLY_FEE + STABLE_MONTHLY_FEE); // adds numbers (works because they're variables)
 console.log(STABLE_MONTHLY_FEE - 20); //subtracts
 console.log(34/5); //divides 
-// *strings will print out as a different colour from *numbers in the terminal
+    // *strings will print out as a different colour from *numbers in the terminal
 console.log("hi" + " there"); //adding two strings together
 console.log("hi" - " there") //can't subtract/divide/multiply strings (things that aren't numbers)
 console.log(80085 + "teehee"); //type corcion
@@ -25,13 +27,14 @@ console.log(420000/1000);
 console.log(`My horses' names are ${horseName} and ${secondHorseName} and they're both ${horseAge * 3}`); //concatenation
 
 
-//LAB for lesson_04
+    //LAB for lesson_04
 let horseNickname = "Jen";
 console.log(`My horses' names are ${horseNickname} and he is ${horseAge * 3}`);
 console.log(`If you stay at our stable for 3 months you would save ${(STABLE_MONTHLY_FEE * 3) * .1} dollars!`);
 
 
-// LESSON-5 Code
+
+    // LESSON-5 Code
 if(isHorseInside) {
 console.log (horseName + " is inside");
 } else {
@@ -66,9 +69,54 @@ if (horseName === "Jenis" && horseAge === 37) {
 }
 
 
-// Lab 5a goes here
+    // Lab 5a goes here
 if(isHorseInside) {
     console.log (horseName + " is inside");
     } else {
         console.log (horseName + " is outside");
     }
+
+
+
+    // Lesson-6 Code
+    // Horse = ["name", age, isInside, ]
+let word = "Rainbow";
+console.log(word.charAt(4));
+    // Answer consoles out as "b" because it starts counting at "0" not "1"
+
+let horseNames = [horseName, "Kenis", "Meep"];
+let horseAges = [36, horseAge, 39];
+let horseLocations = [true, true, true, true];
+let crazyArray = [isHorseInside, "hi", "there", [[1, 2, 3, 2, 3]]]; 
+console.log(horseNames.length);
+console.log(horseNames);
+horseNames.push("Beep");
+console.log(horseNames);
+horseAges.splice(2, 0, 38); 
+    // we don't need to know splice (first number is the start position, second number is what you're doing with the number (0 = insert, 1 = replace), third number is what you're adding (a number, or "text"))
+console.log(horseAges);
+console.log(`Welcome to my stables! There are ${horseNames.length} horses staying here! Their names are: ${horseNames}`);
+console.log("Welcome to my stables! There are " + horseNames.length + " horses staying here");
+console.log(horseNames[1] + " is");
+console.log(horseAges[1]);
+
+if(horseLocations);{
+    console.log(horseNames[2] + " is outside!");
+}
+
+if(horseNames[0] === "Jenis") {
+console.log("Hi Charlie!");
+}
+
+if(horseNames.length < 5){
+    console.log("These stables are unpopular");
+}
+
+
+// LAB for lesson-6
+console.log(`Welcome to Jenis' stables! There are ${horseNames.length} horses staying here! Their names are: ${horseNames[0]}, ${horseNames[1]}, ${horseNames[2]}, and, ${horseNames[3]}`);
+
+if(horseLocations[0] && horseLocations[1] && horseLocations[2] && horseLocations[3] === true) {
+    console.log(horseNames + " are all chilling inside!");
+} else {console.log(horseNames + " went for a run.");}
+
